e080d94d8a2f2817206320f603328bccac51d821
JaceyKarto <jaceykarto@gmail.com>
2018-05-27 15:19:07 +0200

    Update test.sh
    PARAMETERS WERKEN, Er worden mappen gemaakt op basis van parameters 2 (Week of Maand)

--- test.sh
+++ test.sh
@@ -4,10 +4,29 @@
 PAR=$2
 MAP=$1
 
-#maakt nieuwe folder op basis van modified date
-for x in *; do
-  d=$(date -r "$x" +%Y-%m-%d)
-  mkdir -p "$d"
-  mv -- "$x" "$d/"
-done
+#Change naar directory die meegegeven wordt in parameter 1
+cd $MAP
 
+#Als parameter 2 gelijk is aan maand, sorteer de mappen op maand.
+if [ $PAR == 'maand' ];then
+
+#maakt nieuwe folder op basis van modified date
+	for x in *; do
+		d=$(date -r "$x" +%m-%Y)
+		mkdir -p "$d"
+		cp -- "$x" "$d/"
+	done
+	echo "Het is met succes uitgevoerd!"
+fi
+
+#Als parameter 2 gelijk is aan week, sorteer de mappen dan op week.
+if [ $PAR == 'week' ];then
+
+#maakt nieuwe folder op basis van modified date
+	for x in *; do
+		d=$(date -r "$x" +%V-%Y)
+		mkdir -p "$d"
+		cp -- "$x" "$d/"
+	done
+	echo "Het is met succes uitgevoerd!"
+fi
